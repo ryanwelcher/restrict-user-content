8b36e1607df943d71a68f47fa9f2ac11de9fa649
Ryan Welcher <ryan.welcher@10up.com>
2016-01-15 22:37:41 -0500

    Standards cleanup and removing PHP4 pass by reference &$this

--- restrict-user-content.php
+++ restrict-user-content.php
@@ -8,14 +8,14 @@
  * Text Domain: ruc
  */
 
-if ( ! class_exists('Restrict_User_Content') ) :
+if ( ! class_exists( 'Restrict_User_Content' ) ) :
 
-//get the base class
+	// Get the base class.
 	if ( ! class_exists( 'RW_Plugin_Base' ) ) {
 		require_once plugin_dir_path( __FILE__ ) . '/_inc/RW_Plugin_Base.php';
 	}
-//get the interface
-	if ( !interface_exists( 'I_RW_Plugin_Base' ) ) {
+	// Get the interface.
+	if ( ! interface_exists( 'I_RW_Plugin_Base' ) ) {
 		require_once plugin_dir_path( __FILE__ ) . '/_inc/I_RW_Plugin_Base.php';
 	}
 
@@ -64,8 +64,8 @@ if ( ! class_exists('Restrict_User_Content') ) :
 
 			//Start your custom goodness
 			add_action( 'pre_get_posts', 				array( $this, 'ruc_pre_get_posts_media_user_only' ) );
-			add_filter( 'parse_query',					array( $this, 'ruc_parse_query_useronly'		) );
-			add_filter( 'ajax_query_attachments_args',	array( $this, 'ruc_ajax_attachments_useronly'		) );
+			add_filter( 'parse_query',					array( $this, 'ruc_parse_query_useronly' ) );
+			add_filter( 'ajax_query_attachments_args',	array( $this, 'ruc_ajax_attachments_useronly' ) );
 
 
 
@@ -86,9 +86,9 @@ if ( ! class_exists('Restrict_User_Content') ) :
 		 */
 		function ruc_pre_get_posts_media_user_only( $query ) {
 
-			if(strpos( $_SERVER[ 'REQUEST_URI' ], '/wp-admin/upload.php' ) !== false ) {
+			if ( strpos( $_SERVER['REQUEST_URI'], '/wp-admin/upload.php' ) !== false ) {
 
-				if ( !current_user_can( 'update_core' ) ) {
+				if ( ! current_user_can( 'update_core' ) ) {
 					$query->set( 'author__in', $this->ruc_create_list_of_user_ids() );
 				}
 			}
@@ -109,8 +109,8 @@ if ( ! class_exists('Restrict_User_Content') ) :
 		 * @link {http://premium.wpmudev.org/blog/how-to-limit-the-wordpres-posts-screen-to-only-show-authors-their-own-posts/}
 		 */
 		function ruc_parse_query_useronly( $wp_query ) {
-			if ( strpos( $_SERVER[ 'REQUEST_URI' ], '/wp-admin/edit.php' ) !== false ) {
-				if ( !current_user_can( 'update_core' ) ) {
+			if ( strpos( $_SERVER['REQUEST_URI'], '/wp-admin/edit.php' ) !== false ) {
+				if ( ! current_user_can( 'update_core' ) ) {
 					$current_user = wp_get_current_user();
 					$wp_query->set( 'author', $current_user->ID );
 				}
@@ -143,9 +143,9 @@ if ( ! class_exists('Restrict_User_Content') ) :
 
 			$current_user = wp_get_current_user();
 
-			$settings = ( $option = get_option($this->_settings_name) ) ? $option : $this->_default_settings;
+			$settings = ( $option = get_option( $this->_settings_name ) ) ? $option : $this->_default_settings;
 			//create the array from the string
-			$users = explode(',', $settings['additional_user_ids'] );
+			$users = explode( ',', $settings['additional_user_ids'] );
 			//add the the current user id to the beginning
 			array_unshift( $users , $current_user->ID );
 			return $users;
@@ -166,18 +166,18 @@ if ( ! class_exists('Restrict_User_Content') ) :
 		 */
 		function rw_plugin_install() {
 
-			if( $this->_has_settings_page ) {
+			if ( $this->_has_settings_page ) {
 
 				//look for the settings
-				$settings = get_option($this->_settings_name);
+				$settings = get_option( $this->_settings_name );
 
-				if(!$settings) {
+				if ( ! $settings ) {
 					add_option( $this->_settings_name, $this->_default_settings );
-				}else{
+				} else {
 
-					if( isset( $_POST[$this->_settings_name] ) ) {
-						$updated_settings = wp_parse_args( $_POST[$this->_settings_name], $this->_default_settings );
-					}else{
+					if ( isset( $_POST[ $this->_settings_name ] ) ) {
+						$updated_settings = wp_parse_args( $_POST[ $this->_settings_name ], $this->_default_settings );
+					} else {
 						$updated_settings = get_option( $this->_settings_name );
 					}
 
@@ -202,7 +202,7 @@ if ( ! class_exists('Restrict_User_Content') ) :
 				add_meta_box(
 					'debug_area', //Meta box ID
 					__( 'Debug', 'ruc' ), //Meta box Title
-					array(&$this, 'rw_render_debug_setting_box'), //Callback defining the plugin's innards
+					array( $this, 'rw_render_debug_setting_box' ), //Callback defining the plugin's innards
 					'settings_page_'.$this->_pagename, // Screen to which to add the meta box
 					'side' // Context
 				);
@@ -212,8 +212,8 @@ if ( ! class_exists('Restrict_User_Content') ) :
 			//-- additional users to allow
 			add_meta_box(
 				'additional_users',
-				__('Additional Media', 'ruc'),
-				array( &$this, 'render_additional_users_meta_box'),
+				__( 'Additional Media', 'ruc' ),
+				array( $this, 'render_additional_users_meta_box' ),
 				'settings_page_'.$this->_pagename, // Screen to which to add the meta box
 				'normal' // Context
 			);
@@ -255,22 +255,22 @@ if ( ! class_exists('Restrict_User_Content') ) :
 		 * @used-by Custom action "rw_plugin_save_options" in the parent class
 		 */
 		function rw_plugin_save_settings() {
-			//lets just make sure we can save
-			if ( !empty($_POST) && check_admin_referer( "{$this->_pagename}_save_settings", "{$this->_pagename}_settings_nonce" ) ) {
-				//save
-				if( isset( $_POST['submit'] ) ) {
+			// Lets just make sure we can save.
+			if ( ! empty( $_POST ) && check_admin_referer( "{$this->_pagename}_save_settings", "{$this->_pagename}_settings_nonce" ) ) {
+				// Save.
+				if ( isset( $_POST['submit'] ) ) {
 					//status message
 					$old_settings = get_option( $this->_settings_name );
-					$updated_settings = wp_parse_args( $_POST[$this->_settings_name], $old_settings );
-					update_option($this->_settings_name, $updated_settings);
-					printf('<div class="updated"> <p> %s </p> </div>', __('Settings Saved', 'ruc' ) );
+					$updated_settings = wp_parse_args( $_POST[ $this->_settings_name ], $old_settings );
+					update_option( $this->_settings_name, $updated_settings );
+					printf( '<div class="updated"><p>%s</p></div>', esc_html__( 'Settings Saved', 'ruc' ) );
 				}
 
-				//reset
-				if( isset( $_POST['reset'] ) ) {
+				// Reset.
+				if ( isset( $_POST['reset'] ) ) {
 					//status message
-					update_option($this->_settings_name, $this->_default_settings );
-					printf('<div class="error"> <p> %s </p> </div>', __('Settings reset to defaults', 'ruc') );
+					update_option( $this->_settings_name, $this->_default_settings );
+					printf( '<div class="error"><p>%s</p></div>', esc_html__( 'Settings reset to defaults', 'ruc' ) );
 				}
 			}
 		}
@@ -279,8 +279,8 @@ if ( ! class_exists('Restrict_User_Content') ) :
 		 * Retrieve the plugin settings
 		 * @return  array Saved settings for this plugin
 		 */
-		function get_settings( ) {
-			$settings = ( $option = get_option($this->_settings_name) ) ? $option : $this->_default_settings;
+		function get_settings() {
+			$settings = ( $option = get_option( $this->_settings_name ) ) ? $option : $this->_default_settings;
 			return $settings;
 		}
 
@@ -289,13 +289,13 @@ if ( ! class_exists('Restrict_User_Content') ) :
 		 * Filters the name of the settings page
 		 * uses the custom filter "rw_settings_page_title"
 		 */
-		function rw_settings_page_title_filter($title) {
-			return __('Restrict User Content Settings', 'ruc');
+		function rw_settings_page_title_filter( $title ) {
+			return esc_html__( 'Restrict User Content Settings', 'ruc' );
 		}
 	}
 
 
-//create an instance of the class
+	// Create an instance of the class.
 	new Restrict_User_Content();
 
 endif;
